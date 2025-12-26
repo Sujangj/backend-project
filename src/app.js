424a4f6ffaa079d0841b963b8416eab6f3323bd9
Sujangj <sujangj17@gmail.com>
2025-12-26 10:56:39 +0530

    All setup configured

--- src/app.js
+++ src/app.js
@@ -13,5 +13,13 @@ app.use(express.json({ limit: "16kb"}))
 app.use(express.urlencoded({ extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+// Routes import
+import userRouter from "./routes/user.router.js";
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
+
+//http://localhost:5000/api/v1/users/register
  
 export {app}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-import dotenv from 'dotenv';
-import connectDB from './db/index.js';
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: './env'
+  path: "./.env"
 });
 
+const app = express();
+
+// middleware
+app.use(express.json());
+
+// connect DB and start server
 connectDB()
-.then(() => {
+  .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server is running on port ${process.env.PORT}`);
-    })
-})
-.catch((error) => { 
+      console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((error) => {
     console.log("MongoDB connection failed:", error);
-});
+  });
