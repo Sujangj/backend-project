aabb27c37d3e616007da113d8f043706eac24ef2
Sujangj <sujangj17@gmail.com>
2025-12-27 14:17:36 +0530

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,27 +6,33 @@ import { ApiResponse } from '../utils/ApiResponse.js';
 
 const registerUser = asyncHandler(async (req, res) => {
     // get user details from frontend
-    const { fullname, username, email, password } = req.body;
-    console.log("email:", email );
+    const { fullName, username, email, password } = req.body;
+    //console.log("email:", email );
     
     //validation - not empty
-    if ([fullname, username, email, password].some(field => field?.trim() === "")
+    if ([fullName, username, email, password].some(field => field?.trim() === "")
     ) {
         throw new ApiError(400, "All fields are required");
 
     }
 
     // check if user already exists: email, username
-    const existedUser = username.findOne({
+    const existedUser = await User.findOne({
         $or: [{ email }, { username }],
     })
     if (existedUser) {
         throw new ApiError(409, "User with email or username already exists");
     }
+    // console.log(req.files);
 
     //check for Images, check for avatar
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+        
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath =  req.files.coverImage[0].path
+        } 
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required");
@@ -42,7 +48,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
     // create user object - create entry in db
     const user = await User.create({
-        fullname,
+        fullName,
         username: username.toLowerCase(),
         email,
         password,
@@ -66,7 +72,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
     res.status(200).json({
         message: "Ok",
-        data: { fullname, username, email }
+        data: { fullName, username, email }
     });
 });
 
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -18,7 +18,8 @@ import fs from 'fs';
             })
 
             //File has been uploaded successfully
-            console.log('File uploaded successfully', response.url);
+            //console.log('File uploaded successfully', response.url);
+            fs.unlinkSync(localFilePath); // Delete the local file after upload
             return response;
         } catch (error) {
             console.error('Error uploading file to Cloudinary:', error);
