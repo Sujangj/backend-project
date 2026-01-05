97c21a63d3e4d78420b3c4a13d08c9499deff832
Sujangj <sujangj17@gmail.com>
2026-01-05 11:20:14 +0530

    add getWatchHistory function to retrieve user's watch history with video owner details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -372,5 +372,49 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
 
 })
 
+const getWatchHistory = asyncHandler(async (req, res) => {
 
-export { registerUser, loginUser, logoutUser, refreshAcessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile };
+    const user = await User.aggregate([
+        {
+            $match: { 
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        },
+    ])
+    return res
+    .status(200)
+    .json(user[0].watchHistory, "watch history fetched successfully")
+
+})
+
+export { registerUser, loginUser, logoutUser,
+     refreshAcessToken, changeCurrentPassword, getCurrentUser,
+      updateAccountDetails, updateUserAvatar, updateUserCoverImage,
+       getUserChannelProfile, getWatchHistory };
